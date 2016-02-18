b95b026d3844d982f58bc922c787631ddf91b029
fei090620 <yzhou3_05@sina.com>
2016-02-18 23:25:51 +0800

    Signed-off-by: fei090620 <yzhou3_05@sina.com>

---
+++ src/FBGame/FBGame.Core/Entities/PointDemo.cs
@@ -0,0 +1,62 @@
+﻿//======================================================================
+//
+//        Copyright (C) 2016 home    
+//        All rights reserved 
+//       
+//        命名空间:  FBGame.Core.Entities
+//        CLR版本:   4.0.30319.42000
+//        创建年份:  2016
+// 
+//        created by home at 2016/2/18 20:33:24
+//        本人博客：http://www.jianshu.com/users/86c7d3a175e3/latest_articles
+//
+//        描述说明：
+//
+//
+//        修改历史：
+//        --------------------------------------------------------------
+//        Name    |    Date   |   Remark                               
+//        --------------------------------------------------------------
+//        
+//        --------------------------------------------------------------
+//
+//        --------------------------------------------------------------
+// 
+//        --------------------------------------------------------------
+//               
+//        注:转载请保留此信息
+//
+//======================================================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBGame.Core.Entities
+{
+    public interface IPointDemo
+    {
+        double Xcoordinate { set; get; }
+        double Ycoordinate { set; get; }
+        IPointDemo GetNextPoint(double dir, double dis);
+        double DistanceTo(IPointDemo point);
+    }
+
+    public class PointDemo : IPointDemo
+    {
+        public double Xcoordinate { get; set; }
+
+        public double Ycoordinate { get; set; }
+
+        public double DistanceTo(IPointDemo point)
+        {
+            return Math.Sqrt(Math.Pow(Xcoordinate - point.Xcoordinate, 2) + Math.Pow(Ycoordinate - point.Ycoordinate, 2));
+        }
+
+        public IPointDemo GetNextPoint(double dir, double dis)
+        {
+            return new PointDemo() { Xcoordinate = Xcoordinate + Math.Cos(dir) * dis, Ycoordinate = Ycoordinate + Math.Sin(dir) * dis };
+        }
+    }
+}
---
+++ src/FBGame/FBGame.UnitTests/FBGame.Core/LineSegmentTest.cs
@@ -0,0 +1,44 @@
+﻿using FBGame.Core.Entities;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace FBGame.UnitTests.FBGame.Core
+{
+    [TestFixture]
+    public class LineSegmentTest
+    {
+        private IPointDemo endpoint;
+        private IPointDemo startpoint;
+
+        [SetUp]
+        public void SetUpforTest()
+        {
+            startpoint = newMockPoint();
+            endpoint = newMockPoint();
+        }
+
+        private IPointDemo newMockPoint()
+        {
+            var point1 = new Mock<IPointDemo>().SetupAllProperties();
+            point1.Setup( z => z.DistanceTo(It.IsAny<IPointDemo>()))
+                .Callback(x=> x = );
+            point1.Setup(x => x.GetNextPoint(It.IsAny<double>(), It.IsAny<double>())).Returns(new PointDemo() { Xcoordinate = 10, Ycoordinate = 10 });
+            return point1.Object;
+        }
+
+        [Test]
+        public void ToListTest_Normal()
+        {
+            endpoint.Xcoordinate = 100;
+            endpoint.Ycoordinate = 100;
+            startpoint.Xcoordinate = 100;
+            startpoint.Ycoordinate = 200;
+
+            LineSegment line = new LineSegment() { StartPoint = startpoint, EndPoint = endpoint, Precision = 1 };
+            int count = line.ToList().Count;
+
+            Assert.AreEqual(100, count);
+        }
+    }
+}
