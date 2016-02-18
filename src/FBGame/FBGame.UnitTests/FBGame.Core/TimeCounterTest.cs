f36f2982ea86356de57e33762854e2603d47831b
zhouyang <yzhou3_05@sina.com>
2016-02-18 17:23:33 +0800

    Signed-off-by: zhouyang <yzhou3_05@sina.com>

---
+++ src/FBGame/FBGame.UnitTests/FBGame.Core/TimeCounterTest.cs
@@ -0,0 +1,49 @@
+﻿//======================================================================
+//
+//        Copyright (C) 2016 Administrator    
+//        All rights reserved 
+//       
+//        命名空间:  adccPC
+//        CLR版本:   4.0.30319.42000
+//        创建年份:  2016
+// 
+//        created by Administrator at 2016/2/18 16:18:54
+//        本人博客：http://www.jianshu.com/users/86c7d3a175e3/latest_articles
+//
+//        描述说明：
+//
+//
+//        修改历史：
+//        --------------------------------------------------------------
+//        Name    |    Date   |   Remark                               
+//        --------------------------------------------------------------
+//        
+//        --------------------------------------------------------------
+//
+//        --------------------------------------------------------------
+// 
+//        --------------------------------------------------------------
+//               
+//        注:转载请保留此信息
+//
+//======================================================================
+
+using NUnit.Framework;
+
+namespace FBGame.UnitTests.FBGame.Core
+{
+    public class when_working_with_the_TimeCounter_start : Specification
+    {
+    }
+
+    public class and_start :
+        when_working_with_the_TimeCounter_start
+    {
+        [Test]
+        public void then_TimeCounter_status_should_be_running()
+        {
+
+        }
+
+    }
+}
